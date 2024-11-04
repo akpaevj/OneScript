663de0ede296a111c1c8858833f0d3b691ca2980
EvilBeaver <ovsiankin.aa@gmail.com>
2024-11-04 21:30:31 +0300

    Префикс лицензии

--- src/ScriptEngine/Libraries/LibraryManager.cs
+++ src/ScriptEngine/Libraries/LibraryManager.cs
@@ -1,11 +1,14 @@
-﻿using OneScript.Commons;
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v.2.0. If a copy of the MPL
+was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using OneScript.Commons;
 using OneScript.Contexts;
 using ScriptEngine.Machine.Contexts;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScriptEngine.Libraries
 {
